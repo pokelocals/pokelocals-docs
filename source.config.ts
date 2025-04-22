ea5b746a7db0874699d03b2007c5d9cea02376cc
Tomáš Vojtěch <tomas.vojtech@gmail.com>
2025-04-23 01:06:55 +0200

    Update documentation and layout for POKÉ LOCALS; enhance content in index.mdx, remove test.mdx, and adjust layout configurations.

--- source.config.ts
+++ source.config.ts
@@ -1,12 +1,20 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { defineDocs, defineConfig } from "fumadocs-mdx/config";
 
 // Options: https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
+  // docs: {
+  //   schema: z.object({
+  //     title: z.string(),
+  //     description: z.string().optional(),
+  //     icon: z.string().optional(),
+  //   }),
+  // },
 });
 
 export default defineConfig({
   mdxOptions: {
-    // MDX options
+    // remarkPlugins: [],
+    // rehypePlugins: [],
   },
 });
--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,4 +1,4 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 /**
  * Shared layout configurations
@@ -11,23 +11,47 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <img
+          alt="logo"
+          width="137"
+          height="32"
+          decoding="async"
+          data-nimg="1"
+          style={{ color: "transparent" }}
+          src="https://app.pokelocals.online/images/logo-locals-red-beta.svg"
+        />
       </>
     ),
   },
   links: [
-    {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
-    },
+    // {
+    //   text: "Documentation",
+    //   url: "/docs",
+    //   active: "nested-url",
+    // },
+    // {
+    //   type: "main",
+    //   text: "Introduction",
+    //   url: "/docs",
+    // },
+    // {
+    //   type: "menu",
+    //   text: "Guide",
+    //   on: "nav",
+    //   items: [
+    //     {
+    //       text: "Organizations",
+    //       // description: "Learn to use Fumadocs",
+    //       url: "/docs/organizations",
+    //       // (optional) Props for Radix UI Navigation Menu item in Home Layout
+    //       // menu: {
+    //       //   className: "row-span-2",
+    //       //   // add banner to navigation menu card
+    //       //   // can be an image or other elements
+    //       //   banner: <div>Banner</div>,
+    //       // },
+    //     },
+    //   ],
+    // },
   ],
 };
